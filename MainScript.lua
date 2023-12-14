721e427ed284013b645a0991c152960d27254c0b
MankaUser <markfokin00@gmail.com>
2023-12-14 15:25:02 +0200

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -84,7 +84,7 @@ local Tabs = {
 --some mobile support, and yea it's only smaller gui
 if UserInputService.TouchEnabled then
     warn("[ManaV2ForRoblox]: mobile user.")
-    CoreGui.Mana.Tabs:FindFirstChild("scalee").Scale = 0.8
+    CoreGui.Mana.Tabs:FindFirstChild("scalee").Scale = 0.7
 else
     warn("[ManaV2ForRoblox]: not mobile user.")
 end
