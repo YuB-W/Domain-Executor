e7a7ab7dc55d3b9d396e3162595b7cdac9e5d734
MankaUser <markfokin00@gmail.com>
2024-01-01 10:42:39 +0200

    its not working..

--- Scripts/8444591321.lua
+++
@@ -1,9 +0,0 @@
--- Bedwars 30v30
-
-local startTick = tick()
-
-if isfile("Mana/Scripts/6872274481.lua") == true then
-    loadstring(readfile("Mana/Scripts/6872274481.lua"))()
-else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Maanaaaa/ManaV2ForRoblox/main/Scripts/6872274481.lua"))()
-end
